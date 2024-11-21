306f4eed8d1bb908f1bdd36a9e33904685132ff3
Timoburnham <timoburnham@gmail.com>
2024-11-21 17:29:50 -0600

    added html file

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
-(
-    import 
-    "net/http"
+
+import (
+	"log"
+	"net/http"
 )
 
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
