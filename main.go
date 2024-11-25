78d5c50bb68219ec5f336cd699fb4a0978d70003
Timoburnham <timoburnham@gmail.com>
2024-11-25 16:42:47 -0600

    added metrics checking functionality

--- main.go
+++ main.go
@@ -1,12 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct{
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Add(1)
+	next.ServeHTTP(w, r)
+
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request){
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request){
+	cfg.fileserverHits.Store(0)
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
+	
+	apiCfg := &apiConfig{}
+	
 	mux := http.NewServeMux()
+
+	
+	
+	mux.HandleFunc("GET /healthz", healthzHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
+
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle( "/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+
+
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -17,5 +60,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	
 
 }
