d8c8e865c669fb7abe8bc8474c62659868e1b4c1
Timoburnham <timoburnham@gmail.com>
2024-11-26 18:06:36 -0600

    refactored metrics check

--- main.go
+++ main.go
@@ -7,49 +7,51 @@ import (
 	"sync/atomic"
 )
 
-type apiConfig struct{
+type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Add(1)
-	next.ServeHTTP(w, r)
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
 
 	})
 }
 
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request){
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	hits := cfg.fileserverHits.Load()
-	fmt.Fprintf(w, "Hits: %d", hits)
+	fmt.Fprintf(w, `<html>
+		<body>
+		  <h1>Welcome, Chirpy Admin</h1>
+		  <p>Chirpy has been visited %d times!</p>
+		</body>
+	  </html>`, hits)
 }
 
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request){
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 }
 
 func main() {
-	
+
 	apiCfg := &apiConfig{}
-	
+
 	mux := http.NewServeMux()
 
-	
-	
-	mux.HandleFunc("GET /healthz", healthzHandler)
-	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle( "/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
-
-
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -60,7 +62,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	
 
 }
